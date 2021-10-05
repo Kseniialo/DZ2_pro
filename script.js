1eef69148b4e96c8f24c69e961e415bd0cab8762
Kseniialo <88728029+Kseniialo@users.noreply.github.com>
2021-10-05 20:31:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function randomNumber (){
+     return Math.floor(Math.random() * 1000000) + 1;
+     
+}
+
+alert( `Случаеное число 0-1000000: ${randomNumber()} !`);
+
+console.log(randomNumber());
